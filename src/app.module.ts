f24f7fa85447bc71ba8717a189fd25c5613f952b
Isaac <metalisaac321@gmail.com>
2020-10-04 00:45:09 -0600

    Install necesary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeederModule } from './seeder/seeder.module';
+import { BookingModule } from './booking/booking.module';
+import { WomenModule } from './women/women.module';
+import { ClinicModule } from './clinic/clinic.module';
+import { ConsumedMedicationModule } from './consumed-medication/consumed-medication.module';
 
 @Module({
-  imports: [],
+  imports: [SeederModule, BookingModule, WomenModule, ClinicModule, ConsumedMedicationModule],
   controllers: [AppController],
   providers: [AppService],
 })
